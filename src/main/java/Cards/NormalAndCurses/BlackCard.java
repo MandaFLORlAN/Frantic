7a0b68e14193e333e344f81aa7554f97e232b602
k2504466 <florian@tierlistei.ch>
2025-09-25 10:25:14 +0300

    (Feat) black cards now trigger events as well

--- src/main/java/Cards/NormalAndCurses/BlackCard.java
+++ src/main/java/Cards/NormalAndCurses/BlackCard.java
@@ -1,9 +1,11 @@
 package Cards.NormalAndCurses;
 
 import Cards.InterfacesGroundclass.Card;
+import Cards.InterfacesGroundclass.SpecialCard;
+import Connector.Connector;
 import Game.GameState;
 
-public class BlackCard extends Card {
+public class BlackCard extends Card implements SpecialCard {
     public BlackCard(int number) {
         super("Black" + ": " + number, number, null);
     }
@@ -11,6 +13,11 @@ public class BlackCard extends Card {
     @Override
     public boolean isPlayable(GameState gs) {
         return gs.getPlayableNumber() == this.getNumber();
+    }
+
+    @Override
+    public void executeSpecialFunction(String executorName, Connector connector, GameState gs) {
+        connector.triggerEvent(executorName);
     }
 }
 
