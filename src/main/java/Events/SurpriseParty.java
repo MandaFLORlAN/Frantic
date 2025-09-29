586ef9750104209e5b637b218ee8d0e68139137b
k2504466 <florian@tierlistei.ch>
2025-09-29 13:07:11 +0300

    (fix) surprise partz can no longer gift cards recieved in the same event

--- src/main/java/Events/SurpriseParty.java
+++ src/main/java/Events/SurpriseParty.java
@@ -4,16 +4,22 @@ import Cards.InterfacesGroundclass.Card;
 import Connector.Connector;
 import Game.GameState;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SurpriseParty extends BaseEvent {
     @Override
     public void executeEvent(Connector connector, String executor, GameState gameState) {
         List<String> players = connector.getAllPlayerNames();
+        List<GiveAwayTriples> giveAwayTriples = new ArrayList<>();
         for (String player : players) {
             String target = connector.getPlayerTargets(player, "SurpriseParty", 1).getFirst();
-            List<Card> card = connector.getCardToGiveAway(player, 1);
-            connector.transferCardFromPlayerToPlayer(card, player, target);
+            List<Card> cards = connector.getCardToGiveAway(player, 1);
+            giveAwayTriples.add(new GiveAwayTriples(cards, target, player));
+        }
+        for (GiveAwayTriples triples : giveAwayTriples) {
+            connector.transferCardFromPlayerToPlayer(triples.cards, triples.giver, triples.target);
         }
     }
+    record GiveAwayTriples(List<Card> cards, String target, String giver) {}
 }
