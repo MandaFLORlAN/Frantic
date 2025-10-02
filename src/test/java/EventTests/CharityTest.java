cb997346aa1246876adc474f8935b0f0b7751675
k2504466 <florian.mosch@seamk.fi>
2025-10-02 15:53:03 +0300

    (test) tests for Charity and Surprise Party

---
+++ src/test/java/EventTests/CharityTest.java
@@ -0,0 +1,34 @@
+package EventTests;
+
+import Connector.TestConnector;
+import Events.Charity;
+import Players.TestHelpingBot;
+import Repository.CardDatabase;
+import TestHelper.TestCaseProvider;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CharityTest {
+
+    @Test
+    public void testEventExecution() {
+        TestConnector connector = TestCaseProvider.getTestGame();
+        TestHelpingBot hasAll = (TestHelpingBot) connector.getPlayers().get("hasAll");
+        TestHelpingBot threeCards = (TestHelpingBot) connector.getPlayers().get("threeCards");
+        TestHelpingBot fiveCards = (TestHelpingBot) connector.getPlayers().get("fiveCards");
+        TestHelpingBot tenCards = (TestHelpingBot) connector.getPlayers().get("tenCards");
+
+        connector.triggerEvent("tenCards",new Charity());
+        assertEquals(CardDatabase.UNIQUE_CARDS-3, hasAll.getCards().size());
+        assertEquals(4, threeCards.getCards().size());
+        assertEquals(6, fiveCards.getCards().size());
+        assertEquals(11, tenCards.getCards().size());
+
+        connector.triggerEvent("hasAll",new Charity());
+        assertEquals(CardDatabase.UNIQUE_CARDS-6, hasAll.getCards().size());
+        assertEquals(5, threeCards.getCards().size());
+        assertEquals(7, fiveCards.getCards().size());
+        assertEquals(12, tenCards.getCards().size());
+    }
+}
---
+++ src/test/java/EventTests/SurprisePartyTest.java
@@ -0,0 +1,34 @@
+package EventTests;
+
+import Connector.TestConnector;
+import Events.SurpriseParty;
+import Players.TestHelpingBot;
+import Repository.CardDatabase;
+import TestHelper.TestCaseProvider;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SurprisePartyTest {
+
+    @Test
+    public void testEventExecution() {
+        TestConnector connector = TestCaseProvider.getTestGame();
+        TestHelpingBot hasAll = (TestHelpingBot) connector.getPlayers().get("hasAll");
+        TestHelpingBot threeCards = (TestHelpingBot) connector.getPlayers().get("threeCards");
+        TestHelpingBot fiveCards = (TestHelpingBot) connector.getPlayers().get("fiveCards");
+        TestHelpingBot tenCards = (TestHelpingBot) connector.getPlayers().get("tenCards");
+
+        connector.triggerEvent("tenCards", new SurpriseParty());
+        assertEquals(CardDatabase.UNIQUE_CARDS, hasAll.getCards().size());
+        assertEquals(5, threeCards.getCards().size());
+        assertEquals(4, fiveCards.getCards().size());
+        assertEquals(9, tenCards.getCards().size());
+
+        connector.triggerEvent("hasAll", new SurpriseParty());
+        assertEquals(CardDatabase.UNIQUE_CARDS, hasAll.getCards().size());
+        assertEquals(7, threeCards.getCards().size());
+        assertEquals(3, fiveCards.getCards().size());
+        assertEquals(8, tenCards.getCards().size());
+    }
+}
