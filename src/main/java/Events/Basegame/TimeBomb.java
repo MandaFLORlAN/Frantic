a20cefa2773a7b699c82f0fa961cdaa0640ff8fe
k2504466 <florian@tierlistei.ch>
2025-11-06 11:22:49 +0200

    (wip) timebomb

---
+++ src/main/java/Events/Basegame/TimeBomb.java
@@ -0,0 +1,12 @@
+package Events.Basegame;
+
+import Connector.Connector;
+import Events.BaseEvent;
+import Game.GameState;
+
+public class TimeBomb extends BaseEvent {
+    @Override
+    public void executeEvent(Connector connector, String executor, GameState gameState) {
+
+    }
+}
