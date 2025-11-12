dfbc2c1fcff0894dd7474a7079a0f452f51792c1
u241064 <florian.moesch2@sbb.ch>
2025-11-12 15:35:26 +0200

    (wip) maaaaaaaaaaaaaaaan

--- src/main/java/JavaFxInterface/JavaFxVersion.java
+++ src/main/java/JavaFxInterface/JavaFxVersion.java
@@ -22,7 +22,7 @@ public class JavaFxVersion extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         launch(args);
-    }
+    }*/
 }
