e8357919dc43da3917431507ec97a8d1a86fa5a1
u241064 <florian.moesch2@sbb.ch>
2025-11-18 12:37:45 +0200

    (wip) home screen

--- src/main/java/JavaFxInterface/App.java
+++ src/main/java/JavaFxInterface/App.java
@@ -19,4 +19,8 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
