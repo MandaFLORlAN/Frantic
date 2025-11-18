88f2ad669e139e4952273cefe3176c38fed38bb5
k2504466 <florian@tierlistei.ch>
2025-11-18 14:17:04 +0200

    (feat) homeScreen can add and remove LogivBots

--- src/main/java/JavaFxInterface/HomeScreenController.java
+++ src/main/java/JavaFxInterface/HomeScreenController.java
@@ -1,6 +1,89 @@
 package JavaFxInterface;
 
-public class HomeScreenController {
-    public void onAddLogicBot1() {}
+import Connector.Connector;
+import Connector.BaseConnector;
+import Players.LogicBots.LogicBot1;
+import Players.Player;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class HomeScreenController {
+    @FXML
+    private VBox botlist;
+
+    private List<Player> players = new ArrayList<>();
+    private Connector connector = new BaseConnector();
+
+    public void onAddLogicBot1() {
+        players.add(new LogicBot1(getRandomName(), connector));
+        loadPlayers();
+    }
+
+    private void loadPlayers() {
+        botlist.getChildren().clear();
+        for (int i = 0; i < players.size(); i++) {
+            Player player = players.get(i);
+            HBox line = new HBox();
+            line.idProperty().set("botLine:" + i);
+            line.getChildren().add(new Label(player.getClass().getSimpleName() + ": " + player.getPlayerName()));
+            Button deleteButton = new Button("delete");
+            deleteButton.setUserData(i);
+            deleteButton.setOnAction(this::deleteLine);
+            deleteButton.getStyleClass().add("deleteButton");
+            line.getChildren().add(deleteButton);
+            botlist.getChildren().add(line);
+        }
+    }
+
+    private void deleteLine(ActionEvent event) {
+        Node node = (Node) event.getSource();
+        Integer index = (Integer) node.getUserData();
+        players.remove(index.intValue());
+        Node line = node.getScene().lookup("botLine:" + index);
+        botlist.getChildren().remove(line);
+        loadPlayers();
+    }
+
+    public void onStartGame() {
+        System.out.println("starting game");
+    }
+
+    private String getRandomName() {
+        Random rand = new Random();
+        String[] words = getWords();
+        boolean isUnique = false;
+        String name = "";
+        while (!isUnique) {
+            name = words[rand.nextInt(words.length)] + " " + words[rand.nextInt(words.length)];
+            isUnique = true;
+            for (Player player : players) {
+                if (player.getPlayerName().equals(name)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+        }
+        return name;
+    }
+
+    private static String[] getWords() {
+        String text = """
+                Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco 
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                non proident sunt in culpa qui officia deserunt mollit anim id est laborum
+                """;
+        String[] words = text.replace("\n","").split(" ");
+        return words;
+    }
 }
