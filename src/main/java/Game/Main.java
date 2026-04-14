0c7bc2fec6d21aabcaf8f7d6cacc79ff1a374d81
Florian Mösch <moscf1@bfh.ch>
2026-04-14 17:18:48 +0200

    cleanup

--- src/main/java/Game/Main.java
+++ src/main/java/Game/Main.java
@@ -1,21 +1,32 @@
 package Game;
 
+import Cards.InterfacesGroundclass.Card;
+import Cards.InterfacesGroundclass.WishCard;
 import Connector.*;
 import Players.HumanPlayer;
 import Players.Player;
 import Players.RandomBot;
+import Repository.CardDatabase;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        List<Player> players = new ArrayList<>();
+        /*List<Player> players = new ArrayList<>();
         Connector connector = new BaseConnector();
         players.add(new RandomBot("Player 0", connector));
         players.add(new RandomBot("Player 1", connector));
         players.add(new RandomBot("Player 2", connector));
         players.add(new HumanPlayer("Human 3", connector));
-        connector.startGame(players);
+        connector.startGame(players);*/
+        Set<Card> wishcards = new HashSet<>();
+        List<Card> allCards = CardDatabase.ALL_CARDS_ONCE_IMMUTABLE;
+        for (Card card : allCards) {
+            if (card instanceof WishCard) wishcards.add(card);
+        }
+        System.out.println("Breakpoint");
     }
 }
